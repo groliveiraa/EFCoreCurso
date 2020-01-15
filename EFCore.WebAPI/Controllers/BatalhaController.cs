58ac3738745009b6aaad528159bd003d3bcf914b
Gustavo Rodrigues <gustavo.oliveira8258@gmail.com>
2020-01-14 22:04:17 -0300

    Controllers Heroi e Batalha e novo migration

---
+++ EFCore.WebAPI/Controllers/BatalhaController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EFCore.Dominio;
+using EFCore.Repo;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BatalhaController : ControllerBase
+    {
+        public readonly HeroiContext _context;
+        public BatalhaController(HeroiContext contexto)
+        {
+            _context = contexto;
+        }
+        // GET: api/Batalha
+        [HttpGet]
+        public ActionResult Get()
+        {
+            try
+            {
+                return Ok(new Batalha());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex}");
+            }
+        }
+
+        // GET: api/Batalha/5
+        [HttpGet("{id}", Name = "GetBatalha")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Batalha
+        [HttpPost]
+        public ActionResult Post(Batalha model)
+        {
+            try
+            {
+                _context.Batalhas.Add(model);
+                _context.SaveChanges();
+
+                return Ok("Batalha Cadastrada!");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex}");
+            }
+        }
+
+        // PUT: api/Batalha/5
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, Batalha model)
+        {
+            try
+            {
+                if (_context.Batalhas.AsNoTracking().FirstOrDefault(h => h.Id == id) != null)
+                {
+                    _context.Update(model);
+                    _context.SaveChanges();
+
+                    return Ok("Batalha Atualizada!");
+                }
+                return Ok("Não Encontrada!");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex}");
+            }
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
